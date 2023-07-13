880f553ccdcd5e7a230eceeb582509972cd0014d
geraldofori <oforige@gmail.com>
2023-07-13 09:54:09 +0000

    few changes to the add empoloyees

--- src/modules/navigation/data/side-nav.data.ts
+++ src/modules/navigation/data/side-nav.data.ts
@@ -14,66 +14,13 @@ export const sideNavSections: SideNavSection[] = [
 export const sideNavItems: SideNavItems = {
     dashboard: {
         icon: 'tachometer-alt',
-        text: 'Dashboard',
+        text: 'Attendance',
         link: '/dashboard',
     },
-    layouts: {
-        icon: 'columns',
-        text: 'Layouts',
-        submenu: [
-            {
-                text: 'Static Navigation',
-                link: '/dashboard/static',
-            },
-            {
-                text: 'Light Sidenav',
-                link: '/dashboard/light',
-            },
-        ],
-    },
-    pages: {
-        icon: 'book-open',
-        text: 'Pages',
-        submenu: [
-            {
-                text: 'Authentication',
-                submenu: [
-                    {
-                        text: 'Login',
-                        link: '/auth/login',
-                    },
-                    {
-                        text: 'Register',
-                        link: '/auth/register',
-                    },
-                    {
-                        text: 'Forgot Password',
-                        link: '/auth/forgot-password',
-                    },
-                ],
-            },
-            {
-                text: 'Error',
-                submenu: [
-                    {
-                        text: '401 Page',
-                        link: '/error/401',
-                    },
-                    {
-                        text: '404 Page',
-                        link: '/error/404',
-                    },
-                    {
-                        text: '500 Page',
-                        link: '/error/500',
-                    },
-                ],
-            },
-        ],
-    },
+
     charts: {
         icon: 'chart-area',
-        text: 'Charts',
+        text: 'Add Employee',
         link: '/charts',
     },
     tables: {
